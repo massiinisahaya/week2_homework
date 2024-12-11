891af4cd2788c538d802a465f17695c19ad679ea
massiinisahaya <biwalake2018@gmail.com>
2024-12-12 08:44:04 +0900

    2nd correction of week2_homework

--- app.py
+++ app.py
@@ -7,13 +7,12 @@ def main():
     topstories = requests.get("https://hacker-news.firebaseio.com/v0/topstories.json").json()[:30]
 
     for story_id in topstories:
+        time.sleep(1)
         story = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json").json()
         if story.get("title") and story.get("url"):
             print({"title": story["title"], "link": story["url"]})
         else:
             print("None")
-
-    time.sleep(1)
 
 
 if __name__ == "__main__":
