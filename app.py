038e458d1b195132986b05a6aa757b35df88a1c3
massiinisahaya <biwalake2018@gmail.com>
2024-12-11 22:18:26 +0900

    submit week2_homework

---
+++ app.py
@@ -0,0 +1,20 @@
+import requests
+import time
+
+
+def main():
+
+    topstories = requests.get("https://hacker-news.firebaseio.com/v0/topstories.json").json()[:30]
+
+    for story_id in topstories:
+        story = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json").json()
+        if story.get("title") and story.get("url"):
+            print({"title": story["title"], "link": story["url"]})
+        else:
+            print("None")
+
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
